f1ef7cd3b55e72b06aaecb3821a6a6984898cfe8
Schneider <142933752+Shnei92@users.noreply.github.com>
2024-08-23 09:32:06 -0400

    Add files via upload

---
+++ Exo 4 phrases.cs
@@ -0,0 +1,24 @@
+﻿string phase1;
+string phrase2;
+string phrase3;
+string phrase4;
+string separateur;
+
+Console.WriteLine("Entrer votre premiere phrase");
+phase1 = Console.ReadLine();
+
+Console.WriteLine("Entrer votre seconde phrase");
+phrase2 = Console.ReadLine();
+
+Console.WriteLine("Entrer votre troisieme phrase");
+phrase3 = Console.ReadLine();
+
+Console.WriteLine("Entrer votre quatrieme phrase");
+phrase4 = Console.ReadLine();
+
+Console.WriteLine("Choisir un séparateur");
+separateur = Console.ReadLine();
+
+
+string lesPhrases = "Bonjour votre phrase complete est:" + " " + phase1 + " " + separateur + phrase2 + " " + separateur + phrase3 + " " + separateur + phrase4 + " " + separateur;
+Console.WriteLine(lesPhrases);
